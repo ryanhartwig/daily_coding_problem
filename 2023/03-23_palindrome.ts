55e39a6f676bebfceef8f9c2033346ec8d2e7f85
Ryan1431 <ryan1431@gmail.com>
2023-03-23 16:53:34 -0400

    Palindrome permutation challenge

---
+++ 2023/03-23_palindrome.ts
@@ -0,0 +1,35 @@
+/* 
+
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Amazon.
+
+Given a string, determine whether any permutation of it is a palindrome.
+
+For example, carrace should return true, since it can be rearranged to form racecar, which is a palindrome. daily should return false, since there's no rearrangement that can form a palindrome.
+
+*/
+
+(() => {
+
+  const canPalindrome = (str: string) => {
+    const chars = new Set();
+    
+    for (let i = 0; i < str.length; i++) {
+      let existing = chars.has(str[i]);
+
+      if (existing) {
+        chars.delete(str[i]);
+      } else {
+        chars.add(str[i]);
+      }
+    }
+
+    return chars.size <= 1;
+  }
+
+  console.log(canPalindrome('carrace')); // true
+  console.log(canPalindrome('daily')); // false
+  console.log(canPalindrome('abccbaf')); // true
+  
+})()

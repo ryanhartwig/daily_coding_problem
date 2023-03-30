39a93d6a46525279c1f79d3f82f97cfa2efae0c3
Ryan1431 <ryan1431@gmail.com>
2023-03-30 17:50:36 -0400

    Fix duplicate challenge to solve for unsorted array input

--- 2023/03-30_duplicate.ts
+++ 2023/03-30_duplicate.ts
@@ -12,14 +12,16 @@ You are given an array of length n + 1 whose elements belong to the set {1, 2, .
 
   const findDuplicate = (arr: number[]) => {
 
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] !== i + 1) {
-        return [arr[i], i] // return the duplicate value, and the index position of the duplicate
-      }
+    const values = new Set<number>();
+
+    for (const number of arr) {
+      if (values.has(number)) return number;
+      
+      values.add(number);
     }
   }
 
-  console.log(findDuplicate([1,2,3,3])); // [3, 3]
-  console.log(findDuplicate([1,2,3,4,4,5])); // [4, 4]
+  console.log(findDuplicate([1,2,3,3])); // 3
+  console.log(findDuplicate([1,2,3,4,4,5])); // 4
   
 })()
